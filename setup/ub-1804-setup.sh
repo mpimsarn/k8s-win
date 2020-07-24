35219a7b35684c316ed2ffc511b030f52b8c9977
Maneesak Pimsarn <mpimsarn@users.noreply.github.com>
2020-07-24 13:51:36 -0500

    k

--- setup/ub-1804-setup.sh
+++ setup/ub-1804-setup.sh
@@ -3,8 +3,8 @@
 # DOCKER_VERSION="5:19.03.0~3-0~ubuntu-bionic"
 # KUBERNETES_VERSION="1.15.1-00"
 
-DOCKER_VERSION="19.03.12"
-KUBERNETES_VERSION="1.18.6"
+DOCKER_VERSION="5:19.03.12~3-0~ubuntu-focal"
+KUBERNETES_VERSION="1.18.6-00"
 
 # turn off swap - for the Kubelet
 swapoff -a 
@@ -21,10 +21,10 @@ apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+# add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
 
 apt-get update
 apt-get install -y \
@@ -34,15 +34,20 @@ apt-get install -y \
 
 # install Kubeadm etc.
 # Note - Bionic packages not available yet
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+# cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+# deb https://apt.kubernetes.io/ kubernetes-xenial main
+# EOF
+
 apt-get update
 apt-get install -y \
-    kubelet=$KUBERNETES_VERSION \
-    kubeadm=$KUBERNETES_VERSION \
-    kubectl=$KUBERNETES_VERSION
+    # kubelet=$KUBERNETES_VERSION \
+    # kubeadm=$KUBERNETES_VERSION \
+    # kubectl=$KUBERNETES_VERSION
+
+    kubelet \
+    kubeadm \
+    kubectl    
 
 # set iptables for Flannel
 sysctl net/bridge/bridge-nf-call-ip6tables = 1
