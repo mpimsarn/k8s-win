de2dd4d61c486ffdb8a595c4639e35686612ce01
Maneesak Pimsarn <mpimsarn@users.noreply.github.com>
2020-07-24 12:19:49 -0500

    add kube-flannel

--- setup/ub-1804-master.sh
+++ setup/ub-1804-master.sh
@@ -11,6 +11,7 @@ POD_NETWORK_CIDR="192.168.0.0/16"
 kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR 
 
 # install Flannel pod network
-wget https://raw.githubusercontent.com/mpimsarn/k8s-win/master/setup/flannel/kube-flannel-hybrid.yaml
+#ipwget https://raw.githubusercontent.com/mpimsarn/k8s-win/master/setup/flannel/kube-flannel-hybrid.yaml
+wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-kubectl --kubeconfig='/etc/kubernetes/admin.conf' apply -f kube-flannel-hybrid.yaml
+#kubectl --kubeconfig='/etc/kubernetes/admin.conf' apply -f kube-flannel-hybrid.yaml
