6c04ae77081268cd8308d4e646ad8a8d9e6e8c37
Maneesak Pimsarn <mpimsarn@users.noreply.github.com>
2020-07-27 18:31:08 -0500

    Update ub-2004-setup.sh

--- setup/ub-2004-setup.sh
+++ setup/ub-2004-setup.sh
@@ -68,7 +68,7 @@ sysctl --system
 # (Install containerd)
 ## Set up the repository
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common net-tools 
 
 ## Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
